6c690215301973c7dbf748bb6f6c1bc845fcba9d
Heriberto Cantu <es.beto@gmail.com>
2020-08-23 11:07:31 -0500

    js: Add nested insideOut option

--- js/object-inside-out.js
+++ js/object-inside-out.js
@@ -3,9 +3,34 @@
 // Learn more about Object.fromEntries() at:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries
 
+// Shallow insideOut
 function insideOut(obj) {
   return Object.fromEntries(Object.entries(obj).map(([key, value]) => [value, key]));
 }
 
-console.log(insideOut({a: 1, b: 2, c: 1}));
+console.log(insideOut({ a: 1, b: 2, c: 1 }));
 // => { '1': 'c', '2': 'b' }
+
+// Nested insideOut
+function insideOutDeep(object) {
+  return Object.entries(object).reduce((acc, [parentKey, parentValue]) => {
+    // parentKey: 'FEE', parentValue { SEVEN: 10, PHARM: 8 }
+
+    for (const [key, value] of Object.entries(parentValue)) {
+      // key: 'SEVEN', value: 10
+
+      acc[key] = acc[key] || {};
+      acc[key][parentKey] = value;
+    }
+    return acc;
+  }, {});
+}
+
+const options = {
+  FEE: { SEVEN: 10, PHARM: 8 },
+  MINIMUM: { SEVEN: 18, PHARM: 0 },
+  MAXIMUM: { SEVEN: 10000, PHARM: 10000 },
+};
+console.log(insideOutDeep(options));
+// => { SEVEN: { FEE: 10, MINIMUM: 18, MAXIMUM: 10000 },
+//      PHARM: { FEE: 8, MINIMUM: 0, MAXIMUM: 10000 } }
